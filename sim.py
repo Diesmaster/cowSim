6c41d1f8978eb72bd21f311e8793ab4a44f84ee6
diesmaster <chris@vanmaarseveen.net>
2023-06-15 19:05:09 +0200

    big steps

--- sim.py
+++ sim.py
@@ -12,12 +12,18 @@ from price import Price_model
 from standard_sim import Standard_sim
 from parser import Parser
 
+# TODO:
+# make cml tool
+# extend capabilities
+# make a boilerplate.py that is copyable to make more models
+# make more models
+
 if not sys.argv[1]:
    print("missing a amount of cycles as first argument and a amount invested as second argument as array")
 if not sys.argv[2]:
    print("amount of cycles should be the first argument, and missing a amount invested as second argument as array")
 
-new_sim = Cow_simulator(int(sys.argv[2]))
+new_sim = Cow_simulator()
 fin_mod = Financial_model(new_sim)
 std_sim = Standard_sim(new_sim)
 
